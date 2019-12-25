3d5b367e74a58e9c1260c61c68bd5eb447021b05
SimonFredrich <21simplex02@gmail.com>
2019-12-25 15:20:55 +0100

    degree bug

--- python-clock/clock.py
+++ python-clock/clock.py
@@ -1,18 +1,31 @@
 import pygame
 import time
+import math
 
 class Clock:
-    def __init__(self, x, y, radius, minute, hour, end_pos, surface):
+    def __init__(self, x, y, radius, degree, screen):
         self.x = x
         self.y = y
-        self.minute = minute
-        self.hour = hour
+        self.degree = degree
         self.radius = radius
-        self.end_pos = end_pos
+        self.screen = screen
 
     def draw(self):
-        pygame.draw.circle(screen, (0, 255, 0, 70), (400, 400), 150)
-        pygame.draw.line(screen, (0, 255, 0), (400, 400), end_pos)
+        if (self.degree > 180):
+            y_pos = self.y + math.tan(self.degree) * self.radius
+        elif self.degree == 180:
+            y_pos = self.y
+        else:
+            y_pos = self.y - math.tan(self.degree) * self.radius
 
-    def update(self):
+
+        if (self.degree < 90 or self.degree > 270):
+            x_pos = self.x + math.cos(self.degree) * self.radius
+        elif self.degree == 90 or self.degree == 270:
+            x_pos = self.x
+        else:
+            x_pos = self.x + math.cos(self.degree) * self.radius
+
+        pygame.draw.circle(self.screen, (0, 255, 0), (self.x, self.y), self.radius)
+        pygame.draw.line(self.screen, (255, 255, 0), (self.x, self.y), (x_pos, y_pos))
 
--- python-clock/main.py
+++ python-clock/main.py
@@ -1,17 +1,27 @@
 import pygame
+import clock
 
 pygame.init()
 
-(width, height) = (400, 400)
+(width, height) = (800, 800)
 background_color = (150, 150, 150)
 
 screen = pygame.display.set_mode((width, height))
+screen.fill(background_color)
 pygame.display.set_caption("python-clock")
+clock_one = clock.Clock(200, 200, 100, 300, screen)
+
+degree_list = []
+for degree in range(0, 360, 6):
+    degree_list.append(degree)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    clock_one.draw()
+
 
     pygame.display.flip()
