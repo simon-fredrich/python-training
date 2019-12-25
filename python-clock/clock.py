7fd729892dc90223164c73a4c1638005e560d5b4
SimonFredrich <21simplex02@gmail.com>
2019-12-25 22:51:56 +0100

    completion of clock

--- python-clock/clock.py
+++ python-clock/clock.py
@@ -1,31 +1,43 @@
 import pygame
-import time
 import math
 
 class Clock:
-    def __init__(self, x, y, radius, degree, screen):
+    def __init__(self, x, y, radius, screen):
         self.x = x
         self.y = y
-        self.degree = degree
         self.radius = radius
         self.screen = screen
 
-    def draw(self):
-        if (self.degree > 180):
-            y_pos = self.y + math.tan(self.degree) * self.radius
-        elif self.degree == 180:
-            y_pos = self.y
+    def draw(self, degree, zeiger):
+        degree = math.radians(degree)
+        if degree > 180:
+            y_pos = self.y + math.sin(degree) * self.radius * zeiger
         else:
-            y_pos = self.y - math.tan(self.degree) * self.radius
+            y_pos = self.y + math.sin(degree) * self.radius * zeiger
 
 
-        if (self.degree < 90 or self.degree > 270):
-            x_pos = self.x + math.cos(self.degree) * self.radius
-        elif self.degree == 90 or self.degree == 270:
-            x_pos = self.x
+        if (degree < 90 or degree > 270):
+            x_pos = self.x + math.cos(degree) * self.radius * zeiger
         else:
-            x_pos = self.x + math.cos(self.degree) * self.radius
+            x_pos = self.x + math.cos(degree) * self.radius * zeiger 
+        
+        return ((x_pos, y_pos))
 
-        pygame.draw.circle(self.screen, (0, 255, 0), (self.x, self.y), self.radius)
-        pygame.draw.line(self.screen, (255, 255, 0), (self.x, self.y), (x_pos, y_pos))
+    def drawClock(self):
+        pygame.draw.circle(self.screen, (0, 255, 0), (self.x, self.y), self.radius, 1)
+        
+    def drawSec(self, degree, zeiger):
+        pygame.draw.line(self.screen, (255, 255, 0), (self.x, self.y), self.draw(degree, zeiger))
+
+    def drawMin(self, degree, zeiger):
+        pygame.draw.line(self.screen, (255, 255, 0), (self.x, self.y), self.draw(degree, zeiger))
+
+    def drawHour(self, degree, zeiger):
+        pygame.draw.line(self.screen, (255, 255, 0), (self.x, self.y), self.draw(degree, zeiger))
+
+    def display(self, sd, md, hd):
+        self.drawClock()
+        self.drawSec(sd, 1)
+        self.drawMin(md, 0.75)
+        self.drawHour(hd, 0.5)
 
--- python-clock/main.py
+++ python-clock/main.py
@@ -1,27 +1,42 @@
 import pygame
 import clock
+import math
+from time import localtime, strftime
 
-pygame.init()
 
-(width, height) = (800, 800)
+(width, height) = (400, 400)
 background_color = (150, 150, 150)
-
 screen = pygame.display.set_mode((width, height))
 screen.fill(background_color)
 pygame.display.set_caption("python-clock")
-clock_one = clock.Clock(200, 200, 100, 300, screen)
 
-degree_list = []
-for degree in range(0, 360, 6):
-    degree_list.append(degree)
+# initializing clock Class
+clock_one = clock.Clock(200, 200, 150, screen)
+
+sec360min = []
+hour2degree = []
+for item in range(-90, 270, 6):
+    sec360min.append(item)
+
+for item in range(-90, 270, 30):
+    hour2degree.append(item)
+
+
+print(hour2degree)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    second = sec360min[int(strftime("%S", localtime()))]
+    minute = sec360min[int(strftime("%M", localtime()))]
+    hour = int(strftime("%H", localtime()))
+    if (int(strftime("%H", localtime())) > 12):
+        hour -= 12
+        hour = hour2degree[hour]
 
-    clock_one.draw()
-
+    screen.fill(background_color)
+    clock_one.display(second, minute, hour)
 
     pygame.display.flip()
