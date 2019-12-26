85cc4f7686ff94949186c78764ed5892cfe91128
SimonFredrich <21simplex02@gmail.com>
2019-12-26 13:36:42 +0100

    enhancment in clock-object generation

--- python-clock/main.py
+++ python-clock/main.py
@@ -3,26 +3,37 @@ import clock
 import math
 from time import localtime, strftime
 
-
-(width, height) = (400, 400)
+pygame.init()
+(width, height) = (800, 800)
 background_color = (150, 150, 150)
 screen = pygame.display.set_mode((width, height))
 screen.fill(background_color)
 pygame.display.set_caption("python-clock")
+font = pygame.font.SysFont("comicsansms", 72)
+text = font.render("The Python Clock", True, (0, 128, 0))
 
-# initializing clock Class
-clock_one = clock.Clock(200, 200, 150, screen)
+
+# creating objects
+clock_arr = []
+i = 0
+j = 0
+for row in range(0, 4):
+    clock_arr.append(clock.Clock(int(width/8)+i, int(width/8), int(width/8), screen))
+    for col in range(0, 4):
+        clock_arr.append(clock.Clock(int(width/8)+i, int(height/8)+j, int(width/8), screen))
+        j += int(width/4)
+    i += int(width/4)
+    j = 0
+
+print(len(clock_arr))
 
 sec360min = []
 hour2degree = []
 for item in range(-90, 270, 6):
     sec360min.append(item)
 
-for item in range(-90, 270, 30):
+for item in range(-60, 300, 30):
     hour2degree.append(item)
-
-
-print(hour2degree)
 
 running = True
 while running:
@@ -32,11 +43,15 @@ while running:
     second = sec360min[int(strftime("%S", localtime()))]
     minute = sec360min[int(strftime("%M", localtime()))]
     hour = int(strftime("%H", localtime()))
-    if (int(strftime("%H", localtime())) > 12):
+    if hour > 12:
         hour -= 12
         hour = hour2degree[hour]
-
+    else:
+        hour = hour2degree[hour-1]
     screen.fill(background_color)
-    clock_one.display(second, minute, hour)
+    for clock in clock_arr:
+        clock.display(second, minute, hour)
+
+    screen.blit(text, (320 - text.get_width() // 2, 240 - text.get_height() // 2))
 
     pygame.display.flip()
