9aac06a3262001cb21d7ee8b5f4fb84068f622ed
SimonFredrich <21simplex02@gmail.com>
2019-12-27 01:49:29 +0100

    trying out tensors and diff np arr

---
+++ pytorch/matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+x = np.array([[1, 4, 6],
+              [3, 5, 2],
+              [7, 2, 9]])
+
+print(x)
+print("A matrix is of rank %d" %(x.ndim))
---
+++ pytorch/scalar.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+x = np.array(42)
+print(x)
+print("A scalar is of rank %d" %(x.ndim))
