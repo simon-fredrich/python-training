2fcae898a2f83ae5974e1ab9e484d5f8b1b5df6c
SimonFredrich <21simplex02@gmail.com>
2020-01-05 00:28:47 +0100

    tutorial 2 init

---
+++ pytorch/tutorial_2.py
@@ -0,0 +1,18 @@
+import torch
+
+# Create tensor with requires_grad=True to track computation
+x = torch.ones(2,2, requires_grad=True)
+print(x)
+
+# compute with x
+y = x + 2
+print(y)
+print(y.grad_fn)
+
+# compute with y
+z = y*y*3
+out = z.mean()
+print(z, out)
+
+out.backward()
+print(x.grad)
