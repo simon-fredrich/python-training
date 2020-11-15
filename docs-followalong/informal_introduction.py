25727f765e870aef8d85e1587237c21a18da2403
SimonFredrich <simon.fredrich@fredax.de>
2020-11-15 01:21:17 +0100

    array indexing

---
+++ docs-followalong/informal_introduction.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: cp1252 -*-
+
+marry_poppins = 'supercalifragilisticexpialidocious'
+print(marry_poppins[9:20]) # fragilistic
+print(marry_poppins[-7:]) # docious
+print(marry_poppins[:13] + marry_poppins[13:])
