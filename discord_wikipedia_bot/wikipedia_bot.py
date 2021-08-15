2009cfaac96be18f8b3c72d8b983952623512fa5
simon-fredrich <21simplex02@gmail.com>
2021-08-15 14:29:38 +0200

    wikipedia info and weather functions

---
+++ discord_wikipedia_bot/wikipedia_bot.py
@@ -0,0 +1,31 @@
+import discord
+import python_weather
+import wikipedia
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("info"):
+        if (message.channel.id == 778335092812021802) or (message.channel.id == 801470078579245056):
+            query = message.content[5:]
+            wikipedia.set_lang("de")
+            await message.channel.send(wikipedia.summary(query, sentences=3))
+            print(query)
+
+    if message.content.startswith("wetter"):
+        if (message.channel.id == 778335092812021802) or (message.channel.id == 801470078579245056):
+            query = message.content[7:]
+            wclient = python_weather.Client(format=python_weather.METRIC)
+            weather = await wclient.find(query)
+            await message.channel.send("In " + query.capitalize() + " sind es " + str(weather.current.temperature) + " Grad.")
+            await wclient.close()
+            
+client.run("ODc2NDA2NDQ1MTExOTA2MzQ1.YRjnGw.M_nKCP9UnrnfhF-4QtcynZjx3lI")
